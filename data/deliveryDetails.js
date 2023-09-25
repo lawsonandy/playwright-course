e0d7e45ea2d76ef12251685617db9677e0e1ec98
Andy Lawson <75863183+lawsonandy@users.noreply.github.com>
2023-09-24 18:55:32 -0700

    Initial commit

---
+++ data/deliveryDetails.js
@@ -0,0 +1,8 @@
+export const deliveryDetails = {
+  firstName: "Andy",
+  lastName: "Lawson",
+  street: "909 NE Tillamook",
+  postcode: "97212",
+  city: "Portland",
+  country: "United States of America"
+}
