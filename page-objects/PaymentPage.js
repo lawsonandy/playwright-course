67c9f190f08cd951f7d28869157642fa8d5ea1fe
Andy Lawson <75863183+lawsonandy@users.noreply.github.com>
2023-09-28 10:45:31 -0700

    assert discount code input value

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -1,13 +1,18 @@
+import { expect } from '@playwright/test'
 export class PaymentPage {
   constructor(page) {
     this.page = page
 
     this.discountCode = page.frameLocator('[data-qa="active-discount-container"]')
                             .locator('[data-qa="discount-code"]')
+    this.discountCodeInput = page.getByPlaceholder('Discount code')
   }
 
   activateDiscount = async () => {
     await this.discountCode.waitFor()
-    await this.page.pause()
+    const code = await this.discountCode.innerText()
+    await this.discountCodeInput.waitFor()
+    await this.discountCodeInput.fill(code)
+    await expect(this.discountCodeInput).toHaveValue(code)
   }
 }
