a941f400589197ee3982cc2bdeb3e5112f7c0f37
Andy Lawson <75863183+lawsonandy@users.noreply.github.com>
2023-10-06 15:04:56 -0500

    remove only from account test

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -3,7 +3,7 @@ import { MyAccountPage } from "./../page-objects/MyAccountPage.js"
 import { getLoginToken } from "./../api-calls/getLoginToken.js"
 import { adminDetails } from "./../data/userDetails.js"
 
-test.only("My Account using cookie injection and mocking network request", async ({ page }) => {
+test("My Account using cookie injection and mocking network request", async ({ page }) => {
   const loginToken = await getLoginToken(adminDetails.username, adminDetails.password)
   
   await page.route("**/api/user**", async (route, request) => {
